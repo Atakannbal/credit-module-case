773d536b34983da270804f1e712ef4fc870b59f1
atakannbal <atakannbal@gmail.com>
2025-06-29 17:16:39 +0300

    chore: comment on repositories regarding @Repository annotation

--- src/main/java/com/creditapi/repository/AppUserRepository.java
+++ src/main/java/com/creditapi/repository/AppUserRepository.java
@@ -3,6 +3,10 @@ package com.creditapi.repository;
 import com.creditapi.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+Spring Data JPA scans for interfaces that extend JpaRepository (or other Spring Data repository interfaces) and automatically registers them as Spring-managed beans.
+This eliminates the need to explicitly annotate them with @Repository.
+*/ 
 public interface AppUserRepository extends JpaRepository<AppUser, String> {
     AppUser findByUsername(String username);
 }
